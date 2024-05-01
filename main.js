d5335a7af62554c504b1bbfa31e1d84d32e3e243
Travis Henderson <TravisHenderson646@gmail.com>
2024-05-01 16:54:01 -0400

    first dev branch commit (json support for maps)

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function testerer() {
 }
 
 function save() {
+    num = document.getElementById("fname").value
     localStorage.setItem("test", num);
 }
 
@@ -13,3 +14,37 @@ function load() {
     num = localStorage.getItem("test");
     document.getElementById("demo").innerHTML = num;
 }
+
+
+
+
+
+function replacer(key, value) {
+    if(value instanceof Map) {
+    return {
+        dataType: 'Map',
+        value: Array.from(value.entries()), // or with spread: value: [...value]
+    };
+    } else {
+    return value;
+    }
+}
+function reviver(key, value) {
+    if(typeof value === 'object' && value !== null) {
+        if (value.dataType === 'Map') {
+        return new Map(value.value);
+        }
+    }
+    return value;
+}
+// example stringification of maps using reviver and replacer
+// const originalValue = [
+//     new Map([['a', {
+//       b: {
+//         c: new Map([['d', 'text']])
+//       }
+//     }]])
+//   ];
+// const str = JSON.stringify(originalValue, replacer);
+// const newValue = JSON.parse(str, reviver);
+// console.log(originalValue, newValue);
